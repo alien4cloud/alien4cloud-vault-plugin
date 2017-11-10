f2ba65e8f155fc653a4c57698e5778bdda457e3e
VU Minh Khang <minh.khang.vu.mkv@gmail.com>
2017-11-10 11:12:57 +0100

    ALIEN-2624 : Configure a Vault in alien4cloud

--- src/main/java/org/alien4cloud/secret/vault/configuration/LDAPConfiguration.java
+++ src/main/java/org/alien4cloud/secret/vault/configuration/LDAPConfiguration.java
@@ -1,18 +1,21 @@
 package org.alien4cloud.secret.vault.configuration;
 
-import javax.validation.constraints.NotNull;
-
+import alien4cloud.ui.form.annotation.FormLabel;
+import alien4cloud.ui.form.annotation.FormProperties;
 import alien4cloud.ui.form.annotation.FormPropertyDefinition;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@FormProperties({ "user", "password" })
 public class LDAPConfiguration {
 
-    @NotNull
+    @FormPropertyDefinition(type = "string", description = "COMMON.USERNAME", isRequired = true)
+    @FormLabel("COMMON.USERNAME")
     private String user;
 
-    @FormPropertyDefinition(type = "string", isPassword = true, isRequired = true)
+    @FormPropertyDefinition(type = "string", description = "COMMON.PASSWORD", isPassword = true, isRequired = true)
+    @FormLabel("COMMON.PASSWORD")
     private String password;
 }
--- src/main/java/org/alien4cloud/secret/vault/configuration/TokenConfiguration.java
+++ src/main/java/org/alien4cloud/secret/vault/configuration/TokenConfiguration.java
@@ -1,13 +1,17 @@
 package org.alien4cloud.secret.vault.configuration;
 
+import alien4cloud.ui.form.annotation.FormLabel;
+import alien4cloud.ui.form.annotation.FormProperties;
 import alien4cloud.ui.form.annotation.FormPropertyDefinition;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@FormProperties({ "token" })
 public class TokenConfiguration {
 
-    @FormPropertyDefinition(type = "string", isPassword = true, isRequired = true)
+    @FormPropertyDefinition(type = "string", description = "ORCHESTRATORS.LOCATIONS.SECRETS.TOKEN", isPassword = true, isRequired = true)
+    @FormLabel("ORCHESTRATORS.LOCATIONS.SECRETS.TOKEN")
     private String token;
 }
