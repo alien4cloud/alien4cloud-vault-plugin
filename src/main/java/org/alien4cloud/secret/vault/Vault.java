df16abbad454810877dbdd2e3128aaba28164dd7
EdgarLGB <lgb_edgar@outlook.com>
2017-12-14 17:18:39 +0100

    ALIEN-2625 Add vault client to be able to authenticate a user with ldap or token

--- src/main/java/org/alien4cloud/secret/vault/Vault.java
+++ src/main/java/org/alien4cloud/secret/vault/Vault.java
@@ -1,11 +1,21 @@
 package org.alien4cloud.secret.vault;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.alien4cloud.secret.ISecretProvider;
+import org.alien4cloud.secret.exception.AuthenticationException;
 import org.alien4cloud.secret.exception.NotSupportedAuthenticationMethod;
 import org.alien4cloud.secret.vault.configuration.LDAPConfiguration;
 import org.alien4cloud.secret.vault.configuration.TokenConfiguration;
 import org.alien4cloud.secret.vault.configuration.VaultConfiguration;
 import org.alien4cloud.secret.vault.constant.AuthenticationMethods;
+
+import com.bettercloud.vault.SslConfig;
+import com.bettercloud.vault.VaultConfig;
+import com.bettercloud.vault.response.AuthResponse;
+
+import alien4cloud.model.secret.SecretAuthResponse;
 
 public class Vault implements ISecretProvider<VaultConfiguration> {
 
@@ -25,4 +35,51 @@ public class Vault implements ISecretProvider<VaultConfiguration> {
             throw new NotSupportedAuthenticationMethod("Authentication method [" + configuration.getAuthenticationMethod() + "] is not supported");
         }
     }
+
+    @Override
+    public SecretAuthResponse auth(VaultConfiguration vaultConfiguration, Object credentials) {
+        TokenConfiguration tokenConfiguration = new TokenConfiguration();
+        final com.bettercloud.vault.Vault vault;
+        try {
+            final VaultConfig vaultConfig = new VaultConfig().address(vaultConfiguration.getUrl());
+            if (vaultConfiguration.getCertificate() != null) {
+                // check the tls verify
+                final SslConfig sslConfig = new SslConfig().clientKeyPemUTF8(new String(Files.readAllBytes(Paths.get(vaultConfiguration.getCertificate()))))
+                        .verify(true).build();
+                vaultConfig.sslConfig(sslConfig);
+            } else {
+                final SslConfig sslConfig = new SslConfig().verify(false).build();
+                vaultConfig.sslConfig(sslConfig);
+            }
+            switch (vaultConfiguration.getAuthenticationMethod()) {
+            case AuthenticationMethods.LDAP:
+                // login with ldap
+                vault = new com.bettercloud.vault.Vault(vaultConfig.build());
+                LDAPConfiguration ldapCredentials = (LDAPConfiguration) credentials;
+                AuthResponse authResponse = vault.auth().loginByLDAP(ldapCredentials.getUser(), ldapCredentials.getPassword());
+                tokenConfiguration.setToken(authResponse.getAuthClientToken());
+                break;
+            case AuthenticationMethods.TOKEN:
+                // auth the given token
+                TokenConfiguration tokenCredentials = (TokenConfiguration) credentials;
+                vault = new com.bettercloud.vault.Vault(vaultConfig.token(tokenCredentials.getToken()).build());
+                vault.auth().lookupSelf();
+                tokenConfiguration.setToken(tokenCredentials.getToken());
+                break;
+            default:
+                throw new NotSupportedAuthenticationMethod("Authentication method [" + vaultConfiguration.getAuthenticationMethod() + "] is not supported");
+            }
+        } catch (Exception e) {
+            throw new AuthenticationException("Authentication fails", e);
+        }
+        VaultConfiguration newVaultConfiguration = new VaultConfiguration();
+        newVaultConfiguration.setAuthenticationMethod(AuthenticationMethods.TOKEN);
+        newVaultConfiguration.setUrl(vaultConfiguration.getUrl());
+        newVaultConfiguration.setCertificate(vaultConfiguration.getCertificate());
+        SecretAuthResponse result = new SecretAuthResponse();
+        result.setConfiguration(newVaultConfiguration);
+        result.setCredentials(tokenConfiguration);
+        return result;
+    }
+
 }
